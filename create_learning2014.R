a4ab8a3d54e9c383582d631470c27c555bb6d612
Sanaa Kadi <73628571+sanaakadi@users.noreply.github.com>
2020-11-06 02:28:42 +0200

    Create create_learning2014.R

---
+++ create_learning2014.R
@@ -0,0 +1,236 @@
+read(learning2014)
+JYTOPKYS2-aineiston metatiedot (syksy 2016)
+
+Kimmo Vehkalahti: ASSIST 2014 - Vaihe 3 (osan 2 lopussa), N=183
+Kurssi: Johdatus yhteiskuntatilastotieteeseen, syksy 2014,
+opetusta ja oppimista koskeva kyselytutkimus, joka tehtiin
+Opettajien akatemian mahdollistamana kv tutkimusprojektina.
+
+Tiedon keruu: 3.12.2014 - 10.1.2015/KV
+Datan perustaminen: 14.1.2015/KV, englanniksi 9.4.2015/KV,
+Imputoitu 4.4.2015: ainoat puuttuvat tiedot eräissä taustoissa.
+
+Id       Identification number
+Survey   Survey_year  Year the survey was done
+Country  Finland
+Aa       varmistamista, että muistaa asiat hyvin
+Ab       kehittymistä ihmisenä
+Ac       tiedon rakentelua tosiasioiden pohjalta
+Ad       kykyä käyttää omaksuttua tietoa
+Ae       uusien asioiden ja tietojen ymmärtämistä
+Af       uusien merkitysten näkemistä asioissa
+ST01      1. Järjestän opiskeluolosuhteeni sellaisiksi, että minun on helppo työskennellä.
+SU02      3. Mietin usein, onko opiskelustani todellisuudessa mitään hyötyä.
+D03       4. Pyrin yleensä ymmärtämään opittavan asian merkityksen.
+ST04      5. Järjestän opiskeluun käyttämäni ajan niin, että voin hyödyntää sen mahdollisimman tarkasti.
+SU05      6. On paljon asioita, joiden opiskelussa minun on keskityttävä vain muistamaan ne ulkoa.
+D06       9. Etsin huolellisesti perusteluja ja näyttöä muodostaakseni omat johtopäätökseni opiskeltavista asioista.
+D07      11. Pyrin vertailemaan eri kursseilla esiin tulleita ajatuksia ja käsitteitä aina kun mahdollista.
+SU08     12. Luen tenttiin yleensä sen verran, kuin uskon riittävän tentin läpäisyyn.
+ST09     14. Olen mielestäni melko järjestelmällinen kerratessani tenttiin.
+SU10     16. Monikaan asia opinnoissani ei ole ollut kovin mielenkiintoinen tai relevantti.
+D11      17. Lukiessani kirjaa tai artikkelia yritän saada tarkasti selville, mitä kirjoittaja yrittää sanoa.
+ST12     18. Olen hyvä tarttumaan opiskeluun aina kun se on tarpeen.
+SU13     19. Monet oppimani asiat jäävät usein irrallisiksi, jolloin ne eivät linkity osaksi laajempaa kokonaisuutta.
+D14      21. Työskennellessäni uuden ja vieraan aiheen parissa vertaan sitä aikaisemmin oppimaani.
+D15      23. Huomaan usein kyseenalaistavani luennoilla käsiteltyjä ja kirjoista lukemiani asioita.
+SU16     25. Keskityn oppimaan juuri ne osat kurssin aiheesta, joiden tiedän olevan ehtona läpäisylle.
+ST17     27. Olen hyvä noudattamaan luennoitsijoiden ohjeita lukemisesta.
+SU18     29. Ihmettelen joskus, miksi ylipäätään päätin hakea tänne opiskelemaan.
+D19      30. Lukiessani pysähdyn aika ajoin miettimään, mitä yritän tekstistä oppia.
+ST20     31. Työskentelen tasaisesti koko periodin ajan, sen sijaan että jättäisin kaiken viime hetkeen.
+SU21     32. En usein oikein tiedä, mikä luennoilla on keskeistä, joten pyrin tekemään mahdollisimman paljon muistiinpanoja.
+D22      33. Jään usein pohtimaan tieteellisten tekstien herättämiä ajatuksia ja niiden keskinäisiä kytkentöjä.
+D23      36. Lukiessani tarkastelen huolellisesti, kuinka yksityiskohdat sopivat kokonaisuuteen.
+SU24     38. Keskityn opiskelussani lähinnä niihin asioihin, jotka tuntuvat liittyvän tehtävien ja tenttien suorittamiseen.
+ST25     40. Suunnittelen yleensä viikko-ohjelmani joko paperilla tai mielessäni.
+SU26     42. Opintoihini kuuluu kursseja joista en ole kiinnostunut, mutta joudun kuitenkin käymään ne.
+D27      43. Ennen kuin alan ratkaista tehtävää tai ongelmaa, koetan selvittää mihin se perustuu.
+ST28     44. Hyödynnän yleensä tehokkaasti koko päivän.
+SU29     45. Minulla on usein vaikeuksia ymmärtää asioita, joita minun on muistettava.
+D30      46. Pohdiskelen mielelläni opiskeluun liittyviä asioita, vaikkei se johtaisikaan mihinkään.
+D31      49. Minulle on tärkeää löytää perusteita väitteille ja nähdä asioiden taustalla oleva syy.
+SU32     51. Pidän siitä, että kurssilla kerrotaan tarkasti, mitä esseissä tai kurssitöissä tulee tehdä.
+Ca       L:kertovat tarkasti, mitä muistiinpanoja on syytä tehdä
+Cb       L:rohkaisevat ajattelemaan ja tuovat esiin omaa ajatteluaan
+Cc       T:opiskelija voi näyttää perehtyneensä oppimateriaaleihin
+Cd       T:luentomuistiinpanot riittävät tentin läpäisemiseen
+Ce       K:osoitetaan hyvin selvästi, mitkä kirjat pitää lukea
+Cf       K:kannustetaan lukemaan runsaasti aiheesta itsenäisesti
+Cg       k:haastavat ja selittävät asioita laajemmin kuin luennot
+Ch       k:sisältävät täsmällisiä faktoja ja helposti opittavia tietoja
+Da       Koen osaavani tilastotiedettä
+Db       Tilastotieteestä tulee olemaan hyötyä työelämässä
+Dc       Olen kiinnostunut ymmärtämään tilastotiedettä
+Dd       Pärjäsin hyvin koulun matematiikassa
+De       Olen kiinnostunut oppimaan tilastotiedettä
+Df       Koen epävarmuutta tilastotieteen tehtävien kanssa
+Dg       Pidän tilastotieteestä
+Dh       Tilastotiede pelottaa minua
+Di       Luotan matemaattisiin taitoihini
+Dj       Tilastotiede on hyödyksi tulevissa opinnoissani
+Age      Age (in years) derived from the date of birth
+Gender   Male = 1  Female = 2
+Years    Year_Study   Student's year of study (Number)
+Courses  How many courses are you enrolled in this semester? (Number)
+Hours    How many hours are you using to study statistic in the week (Number)
+Degree   Which is the degree (Insert Name) 1=SocSci,2=other
+Lar      Learning as Reproducing   ~Aa+Ac+Ad
+Lat      Learning as Transforming  ~Ab+Ae+Af
+d_sm     Seeking Meaning           ~D03+D11+D19+D27
+d_ri     Relating Ideas            ~D07+D14+D22+D30
+d_ue     Use of Evidence           ~D06+D15+D23+D31
+su_lp    Lack of Purpose           ~SU02+SU10+SU18+SU26
+su_um    Unrelated Memorising      ~SU05+SU13+SU21+SU29
+su_sb    Syllabus-boundness        ~SU08+SU16+SU24+SU32
+st_os    Organized Studying        ~ST01+ST09+ST17+ST25
+st_tm    Time Management           ~ST04+ST12+ST20+ST28
+Deep     Deep approach             ~d_sm+d_ri+d_ue
+Surf     Surface approach          ~su_lp+su_um+su_sb
+Stra     Strategic approach        ~st_os+st_tm
+Deep_adj Deep_adjusted             ~Deep/12
+Surf_adj Surface_adjusted          ~Surf/12
+Stra_adj Strategic_adjusted        ~Stra/8
+Su_Ti    Transmitting information (related to Surf) ~Ca+Cd+Ce+Ch
+D_Su     Supporting understanding (related to Deep) ~Cb+Cc+Cf+Cg
+StatConf Confidence in doing statistics    ~Da+Df
+Value    Value of statistics               ~Db+Dj
+Interest Interest in statistics            ~Dc+De
+MathConf Confidence in doing math          ~Dd+Di
+Affect   Affect toward statistics          ~Dg+Dh
+Attitude Global attitude toward statistics ~Da+Db+Dc+Dd+De+Df+Dg+Dh+Di+Dj
+FSVdeep  Factor score (Varimax rotation) Deep
+FSVstra  Factor score (Varimax rotation) Strategic
+FSVsurf  Factor score (Varimax rotation) Surface
+FSOdeep  Factor score (Oblimin rotation) Deep
+FSOstra  Factor score (Oblimin rotation) Strategic
+FSOsurf  Factor score (Oblimin rotation) Surface
+Points   Yhteispisteet (max kaikista)
+Fails    Hylkyjen lkm ennen läpäisyä (toimii paremmin Italian datassa!)
+
+
+Mittareiden taustaa:
+
+Mittarit A ja C vastaavat osia A ja C ASSIST-lomakkeessa
+(Approaches and Study Skills Inventory for Students)
+http://www.etl.tla.ed.ac.uk/publications.html#measurement
+ja D:n pohjana on SATS (Survey of Attitudes Toward Statistics)
+http://www.evaluationandstatistics.com/
+
+Tutkimuksessa keskeisimmällä sijalla olleen mittarin (ASSIST B)
+osiot on nimetty siten, että niistä pystyy näkemään yhteyden
+ajateltuun ulottuvuuteen (Deep/SUrface/STrategic). ASSIST:in
+alkuperäisen mittarin (52 osiota) sijaan tutkimuksessa käytettiin
+ryhmän aiemmin lyhentämää versiota, johon kuului vain 32 osiota.
+
+Mittarien A,C,D suomennokset: Kimmo Vehkalahti ja Liisa Myyry.
+Mittarin B suomennos (alun perin): Miia Erkkilä (2008), ks.
+http://lib.tkk.fi/Raportit/2009/isbn9789512297719.pdf
+(pieniä säätöjä sanamuotoihin / KV & LM & Reijo Sund 2014).
+
+Taustoista osa on muiden maiden kanssa yhteisiä, osa omia.
+JYTOPKYS2-data.txt sisältää kaikkien 183 vastaajan tiedot.
+Opiskelijanumero on korvattu anonyymilla tunnisteella (Id).
+Puuttuvat tiedot on merkitty koodilla "NA" (Not Applicable).
+
+Aineisto tulee vapaaseen opetuskäyttöön syksyn 2016 kurssilla
+sekä uudella, 1/2017 alkavalla englanninkielisellä kurssilla
+Introduction to Open Data Science.
+
+Alustavia tuloksia (esitetty 60. ISI-kokouksessa Riossa 7/2015):
+
+http://www.slideshare.net/kimmovehkalahti/the-relationship-between-learning-approaches-and-students-achievements-in-an-introductory-statistics-course-in-finland
+
+--------------------------------------------------------------
+
+Summamuuttujien muodostamissäännöt (A, B, C, D) ym. ohjeita
+in English (see the web links above for more information):
+
+(VAR expressions are related to Survo R, which was used for
+computations, calculations, analyses, and visualizations.)
+
+*/ACTIVATE +   / (in Survo, all other lines are just comments)
+*
++VAR Lar:1=Aa+Ac+Ad    TO ASSIST2015FI
++VAR Lat:1=Ab+Ae+Af
+*
+*Lar       Aa + Ac + Ad
+*Learning as Reproducing
+*Lat       Ab + Ae + Af
+*Learning as Transforming
+*
++VAR d_sm:1=D03+D11+D19+D27
++VAR d_ri:1=D07+D14+D22+D30
++VAR d_ue:1=D06+D15+D23+D31
++VAR su_lp:1=SU02+SU10+SU18+SU26
++VAR su_um:1=SU05+SU13+SU21+SU29
++VAR su_sb:1=SU08+SU16+SU24+SU32
++VAR st_os:1=ST01+ST09+ST17+ST25
++VAR st_tm:1=ST04+ST12+ST20+ST28
++VAR Deep:1=d_sm+d_ri+d_ue
++VAR Surf:1=su_lp+su_um+su_sb
++VAR Stra:1=st_os+st_tm
++VAR Deep_adj:4=Deep/12
++VAR Surf_adj:4=Surf/12
++VAR Stra_adj:4=Stra/8
+*
+*d_sm      D03 + D11 + D19 + D27
+*Seeking Meaning
+*d_ri      D07 + D14 + D22 + D30
+*Relating Ideas
+*d_ue      D06 + D15 + D23 + D31
+*Use of Evidence
+*su_lp     SU02 + SU10 + SU18 + SU26
+*Lack of Purpose
+*su_um     SU05 + SU13 + SU21 + SU29
+*Unrelated Memorising
+*su_sb     SU08 + SU16 + SU24 + SU32
+*Syllabus-boundness
+*st_os     ST01 + ST09 + ST17 + ST25
+*Organized Studying
+*st_tm     ST04 + ST12 + ST20 + ST28
+*Time Management
+*Deep      d_sm + d_ri + d_ue (min = 12, max = 60)
+*Deep approach
+*Surface   su_lp + su_um + su_sb (min =12, max = 60)
+*Surface approach
+*Strategic           st_os + st_tm (min = 8, max = 40)
+*Strategic approach
+*Deep_adjusted       Deep/12 (min = 1, max = 5) - previous Deep score divided by the number of the items of the scale
+*Surface_adjusted    Surface/12 (min = 1, max = 5) - previous Surface score divided by the number of the items of the scale
+*Strategic_adjusted  Strategic/8 (min = 1, max = 5) - previous Strategic score divided by the number of the items of the scale
+*
++VAR Su_Ti:1=Ca+Cd+Ce+Ch
++VAR D_Su:1=Cb+Cc+Cf+Cg
+*
+*Su_Ti     Ca + Cd + Ce + Ch
+*Supporting understanding (related to a deep approach)
+*D_Su      Cb + Cc + Cf + Cg
+*Transmitting information (related to a surface approach)
+*
+*Obs!! Df and Dh must be reversed first:
+*
++VAR Df:1=6-Df
++VAR Dh:1=6-Dh
+*
++VAR StatConf:1=Da+Df
++VAR Value:1=Db+Dj
++VAR Interest:1=Dc+De
++VAR MathConf:1=Dd+Di
++VAR Affect:1=Dg+Dh
++VAR Attitude:1=Da+Db+Dc+Dd+De+Df+Dg+Dh+Di+Dj
+*
+*Stat_Conf           Da + Df
+*Confidence in doing statistics
+*Value     Db + Dj
+*Value of statistics
+*Interest  Dc + De
+*Interest in statistics
+*Math_Conf           Dd + Di
+*Confidence in doing math
+*Affect    Dg + Dh
+*Affect toward statistics
+*Attitude  Da + Db + Dc + Dd + De + Df + Dg + Dh + Di + Dj
+*Global attitude toward statistics
+*
++
