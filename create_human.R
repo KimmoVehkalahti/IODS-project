678f70f48430a973836bbfecd7e6a94b99a6797c
Liyuan E <liyuan.e@helsinki.fi>
2020-11-27 02:20:12 +0200

    the fifth homework

--- create_human.R
+++ create_human.R
@@ -34,3 +34,46 @@ dim(human_Gender)
 
 #save the data
 write.csv(human,file="data/human.csv", row.names = F)
+
+
+
+# Load the 'human' data (Continue the data wrangling last week)
+
+human_Gender <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human2.txt", stringsAsFactors = F)
+
+# Exploring the structure and dimension of the data (My name: Liyuan E; Description: the dimension of the dataset showed that it has 195 observations and 19 variables; the structure of the dataset showed the 19 variables is as following: hdi_rank, country, hdi, life_exp, exp_edu, mean_edu, gni, gni_minus_hdi, gii, mat_mortality, adol_birth, parl_seats,f_2edu, m_2edu, f_lab, m_lab, edu2FMratio and lab2FMratio)
+
+
+str(human_Gender)
+dim(human_Gender)
+
+# Mutate the data
+
+library(stringr)
+str(human_Gender$gni)
+str_replace(human_Gender$GNI, pattern=",", replace ="") %>% as.numeric
+
+# Exclude unneeded variables
+
+keep <- c("country", "edu2FMratio", "lab2FMratio", "life_exp", "exp_edu", "gni", "mat_mortality", "adol_birth", "parl_seats")
+library(dplyr)
+human_Gender <- dplyr::select(human_Gender, one_of(keep))
+
+# remove all rows with missing values
+
+complete.cases(human_Gender)
+human_Gender_ <- filter(human_Gender, complete.cases(human_Gender))
+
+# remove the observations regards regions instead of countries (have to change the name that has to be same above)
+
+tail(human, 10)
+last <- nrow(human) - 7
+human_Gender_ <- human[1:last, ]
+
+# define the row names of the data by the country names and remove the country name column
+
+rownames(human_Gender) <- human_Gender$country 
+human_Gender <- dplyr::select(human_Gender, - country)
+
+# save the data
+write.csv(human_Gender,file="data/human.csv", row.names = F)
