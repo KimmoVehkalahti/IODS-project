a5d605c6d031d06a29a9075a700ca5f0ae1a1e25
iamsubam <117269809+iamsubam@users.noreply.github.com>
2022-11-28 00:41:53 +0200

    commit start

---
+++ creat_human.R
@@ -0,0 +1,10 @@
+
+#Name: Subam Kathet
+
+#Date: 27/11/2022
+
+#Introduction to open data science
+
+#Week 4: Clustering and Classification
+
+#This weeks learning tasks includes exercises from week 4 - clustering and classification 
