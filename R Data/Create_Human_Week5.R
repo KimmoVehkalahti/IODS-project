736600b8a4e1aca86aea9cbf7aa5db34c9132d7f
iamsubam <117269809+iamsubam@users.noreply.github.com>
2022-12-06 12:58:09 +0200

    cor

--- R Data/Create_Human_Week5.R
+++ R Data/Create_Human_Week5.R
@@ -7,11 +7,11 @@
 #We will be working with the same human data wrangled during last weeks exercise session
 #The csv file was saved in the project folder
 
-#Human data originates from United Nations Developement Program
+#Human data originates from United Nations Development Program
 #Link: https://hdr.undp.org/data-center/human-development-index#/indicies/HDI  
 
 #Overview of the data
-      ##calculating the human developement indices-graphical presentation
+      ##calculating the human development indices-graphical presentation
       # https://hdr.undp.org/system/files/documents//technical-notes-calculating-human-development-indices.pdf
 
 #Lets start the data wrangling exercise
@@ -35,21 +35,53 @@ summary("human")
 library(stringr)
 str(human$GNI)
 str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+#Check data now
 summary(human$GNI)
 print(human$GNI)
 
-##Step 3## Exclude unneded variables
-#keep only the columns matching the following variable names (described in the meta file above): 
-#"Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F"
+##Step 3## Exclude unneeded variables
+#keep only the columns matching the following variable names
 
-##Step 4## Remove all rows with missing values
+library(dplyr)
+# columns to keep
+keep <- c("Country", "SeEdu_FM", "LFR_FM", "Life_Exp", "Exp_Edu", "GNI", "MMR", "ABR", "%PR")
+human <- select(human, one_of(keep))
+include <- complete.cases(human)
+
+
+#Lets see now
+str(human); dim(human)
+#195 observation and 8 variables, great !! lets continue
+
+
+##Step 4## Remove all rows with missing values  
+
+data.frame(human[-1], comp = include)
+
+human_ <- filter(human, include)
+
+rownames(human_) <- human_$Country
 
 ##Step 5## Remove the observations which relate to regions instead of countries
 
-##Step 6## Define data
-#Define the row names of the data by the country names and remove the country name column from the data
+tail(human_, n = 10)
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
 
-##Step 7## SAve the file
+##Step 6## #Define the row names of the data by the country names and remove the country name column from the data
+human_
+human_$GNI <- gsub(",", "", human_$GNI) %>% as.numeric
+str(human_);dim(human_)
+
+#155 observations and 7 variables, great !! 
+
+##Step 7## Save the file
+#save new data set to project folder
+write.csv(human_, file="human_.csv")
+
+#check
+read.csv('human_.csv', row.names = 1)
 
 #Data wrangling complete !! lets move on to the analysis 
 
